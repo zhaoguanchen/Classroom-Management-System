8ddeddd3a42f4e0e1e5b4315434acb0bc0b5ca8e
139634zgc <13001253655@163.com>
2017-06-04 19:53:25 +0800

    更改学生信息

---
+++ man_change_sinfo_2.php
@@ -0,0 +1,29 @@
+
+<head>
+	
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>学生登录</title>
+</head>
+
+<?php
+
+ if(isset($_POST["submit"]) )  
+  
+  {
+      $id = $_POST["name"];
+      $number = $_POST["number"];
+	  $class = $_POST["class"];
+      $tel = $_POST["tel"];
+	  $relname = $_POST["relname"];
+   
+   
+include "./includes/mysql_connect.php";  
+			 $sql = "UPDATE `student` SET `class`='$class', `tel`=$tel, `relname`='$relname',`number`='$number' WHERE  `name`='$id'";
+			$result = mysql_query($sql);    //执行SQL语句  
+			$num = mysql_num_rows($result);
+		
+         if($result)  {  echo "<script> alert('修改成功,请及时通知该学生！');location.href='man_main.php'</script>";    }
+		   else{ echo "<script> alert('修改失败！请确认信息正确性');window.history.go(-1);</script>";  }  
+	  }
+ 
+?>
